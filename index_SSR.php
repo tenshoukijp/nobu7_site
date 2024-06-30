94f94068462e75303b94f513db7775b68c9169ab
tenshoukijp <komiyama_a@hotmail.com>
2024-06-30 10:01:32 +0900

    common化したのでここからはなくなった

--- index_SSR.php
+++ index_SSR.php
@@ -4,13 +4,13 @@ require( "contents.php" );
 
 $punnyAddress = "xn--petw8uc11b.jp";
 
-$defaultHomePage = "nobu_ssr_home";
-
-$indexFileName = "index.html";
-
 $strTitle = "将星録.jp";
 
 $strDescription = "将星録の改造";
+
+$defaultHomePage = "nobu_ssr_home";
+
+$indexFileName = "index.html";
 
 require( "common.php");
 ?>
