393c5c781d484417ec240f7858529afcf0ca1101
tenshoukijp <komiyama_a@hotmail.com>
2025-07-22 07:03:41 +0900

    description対応や、bootstrap4→5の修正もれ

--- sitemap.bat
+++ sitemap.bat
@@ -1,5 +1,5 @@
 curl -s https://xn--petw8uc11b.jp/sitemap.php
 timeout /t 5
 
-cmd /C C:/usr/nextftp/NEXTFTP_CLI.EXE $Host13 -local="G:/repogitory_jp/nobu7_site" -quit -nosound -minimize -download=sitemap.xml -nokakunin
+cmd /C C:/usr/nextftp/NEXTFTP_CLI_NO_FOCUS.EXE $Host13 -local="G:/repogitory_jp/nobu7_site" -quit2 -nosound -minimize -download=sitemap.xml -nokakunin
 
